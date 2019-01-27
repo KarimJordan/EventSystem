01a3a1e451dd90a58ad364c06bff34499af18aa2
KarimJordan <karim_ekit@yahoo.com.ph>
2019-01-27 22:02:51 +0800

    Added detail and form .vue files.

--- event/js/main.js
+++ event/js/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 
 import eventList from './components/event-list.vue'
+import eventDetail from './components/event-detail.vue'
+import eventForm from './components/event-form.vue'
 
 new Vue({
     el: '#body',
     components: {
-        eventList
+        eventList,
+        eventDetail,
+        eventForm
     }
 })
